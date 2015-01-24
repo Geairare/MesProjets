f036b0887b4a28b929d54fdfc4948cbf5859c69b
Geairare <gco.alias@laposte.net>
2015-01-24 19:08:04 +0100

    Création

---
+++ Tests/MonProjetTest/src/main/java/paquetage/PPrincipal.java
@@ -0,0 +1,17 @@
+package paquetage;
+
+import org.apache.log4j.Logger;
+
+
+public class PPrincipal {
+	static	Logger logger = Logger.getLogger(PPrincipal.class.getName()) ;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	     logger.info("Entering application.");
+	     logger.info("Exiting application.");
+
+	}
+
+}
